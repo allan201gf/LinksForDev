956a26d5d1332b7dfa0dd00c30f1fb246f24d25a
allan201gf <allan201gf@gmail.com>
2022-01-16 17:16:57 -0300

    Initial commit

---
+++ backend/src/main/java/br/com/allangf/linksfordev/LinksForDevApplication.java
@@ -0,0 +1,13 @@
+package br.com.allangf.linksfordev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinksForDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinksForDevApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/allangf/linksfordev/LinksForDevApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.allangf.linksfordev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinksForDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
